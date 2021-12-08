d2ba663b826b014663e0b5d4a0b4314bad4f32f6
Subir Bhakat <smartsubir2010@gmail.com>
2021-12-08 23:30:38 +0530

    Updates

--- assets/products/convertImages.py
+++
@@ -1,7 +0,0 @@
-from glob import glob                                                           
-import cv2 
-pngs = glob('./**/*.jpg')
-
-for j in pngs:
-    img = cv2.imread(j)
-    cv2.imwrite(j[:-3] + 'jpeg', img)
